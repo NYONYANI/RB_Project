832e0d7e7326436fbb910bac48567de206dc96c9
성훈 <wjdtjdgns4@naver.com>
2024-04-04 17:58:42 +0900

    전부 변경

--- RBcontrol.py
+++ RBcontrol.py
@@ -1,24 +1,69 @@
 import socket
 
-# IP address and ports
-ip_address = '10.0.2.7'
-send_port = 5000
-receive_port = 5001
+# 로봇의 IP 주소와 포트 번호
+robot_ip = '10.0.2.7'
+command_port = 5000
+data_port = 5001
 
-# Create a socket object
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# 로봇에게 명령을 전송하는 함수
+def send_command(command):
+    try:
+        # TCP 소켓 생성
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
+        # 로봇에 연결
+        sock.connect((robot_ip, command_port))
+        
+        # 명령 전송
+        sock.sendall(command.encode())
+        
+        # 소켓 닫기
+        sock.close()
+        
+    except Exception as e:
+        print(f'Error sending command: {e}')
 
-try:
-    # Connect to the robot arm
-    client_socket.connect((ip_address, send_port))
-    print("Connected to the robot arm!")
+# 데이터를 수신하는 함수
+def receive_data():
+    try:
+        # TCP 소켓 생성
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
+        # 로봇에 연결
+        sock.connect((robot_ip, data_port))
+        
+        # 데이터 수신
+        data = sock.recv(1024)
+        
+        # 수신한 데이터 처리
+        # TODO: 데이터 처리 로직을 구현하세요.
+        print(f'Received data: {data.decode()}')
+        
+        # 소켓 닫기
+        sock.close()
+        
+    except Exception as e:
+        print(f'Error receiving data: {e}')
 
-    # Send data to the robot arm
-    # ...
+# 연결 확인 함수
+def check_connection():
+    try:
+        # TCP 소켓 생성
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
+        # 로봇에 연결 시도
+        result = sock.connect_ex((robot_ip, command_port))
+        
+        if result == 0:
+            print('Connection successful')
+        else:
+            print('Connection failed')
+        
+        # 소켓 닫기
+        sock.close()
+        
+    except Exception as e:
+        print(f'Error checking connection: {e}')
 
-except ConnectionRefusedError:
-    print("Failed to connect to the robot arm.")
-
-finally:
-    # Close the socket
-    client_socket.close()
+# 연결 확인 예시
+check_connection()
