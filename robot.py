7ef3d139e82d0f7814bfa4576729ecf4a96e672d
성훈 <wjdtjdgns4@naver.com>
2024-04-12 17:43:11 +0900

    테스트 수정

--- robot.py
+++ robot.py
@@ -16,9 +16,8 @@ def Receive_data(sock):
     try:
         send_command(sock, "reqdata")
         receive_data = sock.recv(1024)  # The total size of the structure
-        unpacked_data = reqdata.RobotData()
-        unpacked_data.unpack(receive_data)
-        print(unpacked_data.jnt_ref)
+        robot_state.unpack(receive_data)
+        #print(robot_state.jnt_ref)
     except Exception as e:
         print(f'Error receiving data: {e}')
 
@@ -38,6 +37,8 @@ def MoveJoint(sock, joint1, joint2, joint3, joint4, joint5, joint6, spd = -1, ac
     send_command(sock, f"jointall {spd}, {acc}, {joint1}, {joint2}, {joint3}, {joint4}, {joint5}, {joint6}")
 
 def MoveTCP(sock, x, y, z, rx, ry, rz, spd = -1, acc = -1):
+    while robot_state.robot_state != 0:
+        receive_data()
     send_command(sock, f"movetcp {spd}, {acc}, {x}, {y}, {z}, {rx}, {ry}, {rz}")
 #tool 작동 코드 전압, d0, d1 로 구성
 def Tool(sock, voltage, d0, d1):
@@ -49,3 +50,7 @@ def ReqData(sock,data_sock):
 
 def pgmode_real(sock):
     send_command(sock,f"pgmode real")
+
+
+
+robot_state = reqdata.RobotData()
